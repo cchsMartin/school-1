5ce7decbdca6eaea8bd7ad36a38f149c88722bc8
Parsistence <66070676+Parsistence@users.noreply.github.com>
2020-10-21 13:28:08 -0400

    Add files via upload

---
+++ ProjectFraction/Fraction.java
@@ -0,0 +1,39 @@
+
+public class Fraction {
+	private int num = 1;
+	private int den = 2;
+	
+	public Fraction() {
+		num = 1;
+		den = 2;
+	}
+	public Fraction(int numerator, int denominator) {
+		num = numerator;
+		den = denominator;
+	}
+	
+	
+	public void sum(int num2, int den2) {
+		int numresult = num * den2 + num2 * den;
+		int denresult = den * den2;
+		System.out.println(num + "/" + den + " + " + num2 + "/" + den2 + " = " + numresult + "/" + denresult);
+	}
+	
+	public void diff(int num2, int den2) {
+		int numresult = num * den2 - num2 * den;
+		int denresult = den * den2;
+		System.out.println(num + "/" + den + " - " + num2 + "/" + den2 + " = " + numresult + "/" + denresult);
+	}
+	
+	public void product(int num2, int den2) {
+		int numresult = num * num2;
+		int denresult = den * den2;
+		System.out.println(num + "/" + den + " * " + num2 + "/" + den2 + " = " + numresult + "/" + denresult);
+	}
+	
+	public void quotient(int num2, int den2) {
+		int numresult = num * den2;
+		int denresult = den * num2;
+		System.out.println(num + "/" + den + " ÷ " + num2 + "/" + den2 + " = " + numresult + "/" + denresult);
+	}
+}
---
+++ ProjectFraction/FractionTest.java
@@ -0,0 +1,21 @@
+
+public class FractionTest {
+
+	public static void main(String[] args) {
+		// Default constructor test (fraction is 1/2):
+		Fraction fr1 = new Fraction();
+		fr1.sum(1, 3);
+		fr1.diff(1, 3);
+		fr1.product(1, 3);
+		fr1.quotient(1, 3);
+		System.out.println();
+		
+		// Custom fraction test (fraction is 2/3):
+		Fraction fr2 = new Fraction(2, 3);
+		fr2.sum(1, 4);
+		fr2.diff(1, 4);
+		fr2.product(1, 4);
+		fr2.quotient(1, 4);
+	}
+
+}
